5a8c54a49ebb8d5805ce3c07f250c1a65666383b
royalman-global <royalmanglobalenterprise@gmail.com>
2024-08-13 17:58:01 +0100

    sitemap

--- Royalman-Website/vite.config.js
+++ Royalman-Website/vite.config.js
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import SitemapPlugin  from 'vite-plugin-sitemap';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    SitemapPlugin({
+      baseUrl: 'https://www.royalmandigitalconcept.com',  // Replace with your domain
+      outDir: './public',                    // Output directory for the sitemap
+      // dynamicRoutes: ['/dynamic-route1', '/dynamic-route2'],  // Add your dynamic routes here if any
+      routes: [
+        {
+          url: '/',
+          lastmod: new Date().toISOString(),
+          priority: 1.0,
+        },
+        {
+          url: '/about',
+          lastmod: new Date().toISOString(),
+          priority: 0.8,
+        },
+        // Add more static routes as needed
+      ],
+    }),
+  ],
+});
